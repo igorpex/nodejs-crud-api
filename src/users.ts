7ba3f386f6d7b6a7221cba4e366fc07d8e6fdb24
igorpex <igor.bogdanov@gmail.com>
2022-06-19 23:44:41 +0300

    refactor: mock users empty array

--- src/users.ts
+++ src/users.ts
@@ -8,10 +8,12 @@ export interface UserWithId extends User {
   id: string;
 };
 
-export const users: UserWithId[] = [{
-  username: "Vasya",
-  age: 12,
-  hobbies: ['paragliding'],
-  id: '985fb843-5f8a-41d4-a715-fe23e6db6a0d'
-}];
+export const users: UserWithId[] = [];
 
+
+// export const users: UserWithId[] = [{
+//   username: "Vasya",
+//   age: 12,
+//   hobbies: ['paragliding'],
+//   id: '985fb843-5f8a-41d4-a715-fe23e6db6a0d'
+// }];
