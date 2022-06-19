1628b2364990ad98b25f90f81779d461f32aa483
igorpex <igor.bogdanov@gmail.com>
2022-06-19 19:43:32 +0300

    refactor: add test users data

---
+++ src/users.ts
@@ -0,0 +1,17 @@
+export interface User {
+  username: string;
+  age: number;
+  hobbies: string[];
+};
+
+export interface UserWithId extends User {
+  id: string;
+};
+
+export const users: UserWithId[] = [{
+  username: "Vasya",
+  age: 12,
+  hobbies: ['paragliding'],
+  id: '985fb843-5f8a-41d4-a715-fe23e6db6a0d'
+}];
+
